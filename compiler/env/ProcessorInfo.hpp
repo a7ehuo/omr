86ca5eb55936452aa96b500973ea391be6aba0bc
Annabelle Huo <Annabelle.Huo@ibm.com>
2024-03-06 16:05:16 -0500

    Add all four prototypes
    Signed-off-by: Annabelle Huo <Annabelle.Huo@ibm.com>

--- compiler/env/ProcessorInfo.hpp
+++ compiler/env/ProcessorInfo.hpp
@@ -187,6 +187,7 @@ enum TR_X86_CPUID_ebx_eax07_ecx00
 inline uint32_t getFeatureFlags8Mask()
    {
    return  TR_HLE
+         | TR_ERMSB
          | TR_RTM
          | TR_AVX2
          | TR_AVX512F
